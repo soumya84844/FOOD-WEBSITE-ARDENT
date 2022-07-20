26580da1169ea5b8dc736c1d3c71df47f9e90537
soumya <soumyadeep.behala2001@gmail.com>
2022-07-20 14:50:04 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
+import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path : 'expenses', component : ExpenseEntryListComponent },
+  { path : 'expenses/detail/:id', component : ExpenseEntryComponent },
+  { path : '', redirectTo : 'expenses', pathMatch : 'full' },
+  { path : 'home', component : HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
 import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
 import { DebugComponent } from './debug/debug.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseEntryComponent,
     ExpenseEntryListComponent,
-    DebugComponent
+    DebugComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
